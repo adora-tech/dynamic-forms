9d14eb68003fa83a8c6d0f43c47ec5737dacfdfa
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-07-04 07:20:05 +0200

    Adapted code changes of master.

--- libs/clarity/src/lib/dynamic-form-input/dynamic-form-dropdown/dynamic-form-dropdown.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { DynamicFormDropdown, DynamicFormInputComponent } from '@dynamic-forms/core';
-
-@Component({
-  selector: 'clr-dynamic-form-dropdown',
-  templateUrl: './dynamic-form-dropdown.component.html'
-})
-export class DynamicFormDropdownComponent extends DynamicFormInputComponent<DynamicFormDropdown> {}
