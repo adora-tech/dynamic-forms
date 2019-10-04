2f064d10893cbfcd83b632dd9cbd5680e824b966
Alexander Gebuhr <AlexanderGebuhr@outlook.com>
2019-10-04 12:53:45 +0200

    Fixed test issues.

--- libs/clarity/src/lib/dynamic-form-input/dynamic-form-textbox/dynamic-form-textbox.component.ts
+++ libs/clarity/src/lib/dynamic-form-input/dynamic-form-textbox/dynamic-form-textbox.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-import { DynamicFormTextbox, DynamicFormInputComponent } from '@dynamic-forms/core';
+import { DynamicFormInputComponent, DynamicFormTextbox, DynamicFormValidationService } from '@dynamic-forms/core';
 
 @Component({
   selector: 'clr-dynamic-form-textbox',
   templateUrl: './dynamic-form-textbox.component.html'
 })
-export class DynamicFormTextboxComponent extends DynamicFormInputComponent<DynamicFormTextbox> {}
+export class DynamicFormTextboxComponent extends DynamicFormInputComponent<DynamicFormTextbox> {
+  constructor(protected validationService: DynamicFormValidationService) {
+    super(validationService);
+  }
+}
